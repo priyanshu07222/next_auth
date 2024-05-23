349bb8721c16444b4c06dbf9e607fb3f125ec2a7
unknown <priyanshu16181389@gmail.com>
2024-05-23 11:18:38 +0530

    created next_auth and done verify and signup routes

---
+++ src/helpers/mailer.ts
@@ -0,0 +1,49 @@
+import User from "@/models/userModel";
+import nodemailer from "nodemailer";
+import bcryptjs from "bcryptjs";
+export const sendEmail = async ({ email, emailType, userId }: any) => {
+  try {
+    const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+
+    // configure mail for usage
+
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000,
+      });
+    }
+
+    var transport = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "76375eadc1d7e2", //wrong hai ye put this in env file
+        pass: "dd2073a0b2c1a8",
+      },
+    });
+
+    const mailOptions = {
+      from: "priyanshu@tiwari.com", // sender address
+      to: "bar@example.com, baz@example.com", // list of receivers
+      subject:
+        emailType === "VERIFY" ? "Verify your email" : "Reset your password", // Subject line
+      // text: "Hello world?", // plain text body
+      html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}"> here </a> to ${
+        emailType === "VERIFY" ? "verify your email" : "reset your password"
+      } or copy and paste the link below in your browser.
+      <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+      </p>`, // html body
+    };
+
+    const mailResponse = await transport.sendMail(mailOptions);
+    return mailResponse;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
---
+++ src/models/userModel.js
@@ -0,0 +1,36 @@
+import { timeStamp } from "console";
+import mongoose, {Schema} from "mongoose";
+import { type } from "os";
+
+const userScheme = new Schema({
+    username: {
+        type: String,
+        required: [true, 'Please provide a username'],
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide an email'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide an email'],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date
+})
+
+const User = mongoose.models.users || mongoose.model("users", userScheme)
+
+export default User;
